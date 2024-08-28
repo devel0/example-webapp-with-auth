813787e23f498ec1dc522649c9ae1354a1257a1a
Lorenzo Delana <lorenzo.delana@gmail.com>
2024-08-29 01:28:30 +0200

    gui automatic login again when Unauthorized 401

--- clientapp/src/axios.manager.ts
+++ clientapp/src/axios.manager.ts
@@ -1,58 +1,66 @@
 import { AuthApi, Configuration, MainApi } from "../api"
-import { API_URL } from "./constants/general"
+import { API_URL, APP_URL_Login, LOCAL_STORAGE_CURRENT_USER_NFO } from "./constants/general"
 import { setGeneralNetwork } from "./redux/slices/globalSlice";
 import axios, { AxiosError, HttpStatusCode } from "axios";
 import { store } from "./redux/stores/store";
 import { setSnack } from "./utils/utils";
 
 export const ConfigAxios = () => {
-    
-    axios.defaults.withCredentials = true
-  
-    axios.interceptors.request.use(
-      async (config) => {                
-        store.dispatch(setGeneralNetwork(true))
-  
-        return config
-      },
-  
-      (error) => {
-        Promise.reject(error)
-      }
-    )
-  
-    axios.interceptors.response.use(
-      (response) => {        
-        store.dispatch(setGeneralNetwork(false))
-  
-        return response
-      },
-  
-      (error: AxiosError) => {        
-        store.dispatch(setGeneralNetwork(false))
 
-        if (error?.response?.status === HttpStatusCode.BadGateway) {
-          setSnack({
-            title: "Network error",
-            msg: ["Backend server unreachable"],
-            type: 'error'
-          })
-  
-          return
+  axios.defaults.withCredentials = true
+
+  axios.interceptors.request.use(
+    async (config) => {
+      store.dispatch(setGeneralNetwork(true))
+
+      return config
+    },
+
+    (error) => {
+      Promise.reject(error)
+    }
+  )
+
+  axios.interceptors.response.use(
+    (response) => {
+      store.dispatch(setGeneralNetwork(false))
+
+      return response
+    },
+
+    (error: AxiosError) => {
+      store.dispatch(setGeneralNetwork(false))
+
+      if (error?.response?.status === HttpStatusCode.Unauthorized) {
+        if (document.location.pathname !== APP_URL_Login) {
+          localStorage.removeItem(LOCAL_STORAGE_CURRENT_USER_NFO)
+          document.location = APP_URL_Login
         }
-        // else handleApiException(error)
-        
-        return Promise.reject(error);
+
+        return
       }
-    )
-  
-  }
+      else if (error?.response?.status === HttpStatusCode.BadGateway) {
+        setSnack({
+          title: "Network error",
+          msg: ["Backend server unreachable"],
+          type: 'error'
+        })
+
+        return
+      }
+      // else handleApiException(error)
+
+      return Promise.reject(error);
+    }
+  )
+
+}
 
 
 export const authApi = new AuthApi(new Configuration({
-    basePath: API_URL()
+  basePath: API_URL()
 }))
 
 export const mainApi = new MainApi(new Configuration({
-    basePath: API_URL()
+  basePath: API_URL()
 }))
