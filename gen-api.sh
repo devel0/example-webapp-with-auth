472da02f0a51fbc50668478ba4b8b28df82e2455
Lorenzo Delana <lorenzo.delana@gmail.com>
2024-10-13 11:05:24 +0200

    path quoting

--- gen-api.sh
+++ gen-api.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-export NODE_OPTIONS=--use-openssl-ca
-
-exdir=$(dirname $(readlink -f "$BASH_SOURCE"))
-
-source "$exdir"/src/app/frontend/.env.development
-
-app_api_spec_url="https://$VITE_SERVERNAME/swagger/v1/swagger.json"
-
 function isUrlAvail() {
     q="$(curl $1 -o /dev/null -w '%{http_code}\n' -s)"
     if [ "$q" == "200" ]; then
@@ -17,31 +9,37 @@ function isUrlAvail() {
     fi
 }
 
-app_api_avail="$(isUrlAvail "$app_api_spec_url")"
+export NODE_OPTIONS=--use-openssl-ca
 
-#--------
+exdir=$(dirname $(readlink -f "$BASH_SOURCE"))
+
+source "$exdir"/src/app/frontend/.env.development
+
+app_api_spec_url="https://$VITE_SERVERNAME/swagger/v1/swagger.json"
+
+#-------- app (typescript)
+
+app_api_avail="$(isUrlAvail "$app_api_spec_url")"
 
 if [ "$app_api_avail" == "1" ]; then
 
-    APIFLD=$exdir/src/app/frontend/api
+    APIFLD="$exdir"/src/app/frontend/api
 
-    rm -fr $APIFLD
+    rm -fr "$APIFLD"
 
-    mkdir $APIFLD
+    mkdir "$APIFLD"
 
-    cd $exdir
+    cd "$exdir"
 
     npx @openapitools/openapi-generator-cli generate \
         -i "$app_api_spec_url" \
         -g typescript-axios \
-        -o $APIFLD
+        -o "$APIFLD"
 
     app_api_generated="1"
 
 fi
 
-if [ "$app_api_generated" != "1" ]; then
+#=================================================
 
-    echo "(W): skip typescript api from $app_api_spec_url"    
-
-fi
+if [ "$app_api_generated" != "1" ]; then echo "(W): skip typescript app api from $app_api_spec_url"; fi
