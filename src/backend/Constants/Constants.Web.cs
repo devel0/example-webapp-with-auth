6e965ff202b0065140c5381565f9d7ab165c11c6
Lorenzo Delana <lorenzo.delana@gmail.com>
2025-04-14 20:58:46 +0200

    code clenaup

--- src/backend/Constants/Constants.Web.cs
+++ src/backend/Constants/Constants.Web.cs
@@ -9,9 +9,7 @@ public static partial class Constants
 
     public const string WEB_Cookie = "Cookie";
     public const string WEB_CookieName_XAccessToken = "X-Access-Token";    
-    public const string WEB_CookieName_XRefreshToken = "X-Refresh-Token";
-
-    public const string WEB_CookieName_XCSRFToken = "X-CSRF-TOKEN";    
+    public const string WEB_CookieName_XRefreshToken = "X-Refresh-Token";    
 
     public const string WEB_HeadersCollection_SetCookie = "Set-Cookie";
 
