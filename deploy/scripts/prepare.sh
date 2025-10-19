0fe4761e514f17c1cf464e7f16859f738378b5d9
Lorenzo Delana <lorenzo.delana@gmail.com>
2025-10-19 17:20:13 +0200

    publish fixes

--- deploy/scripts/prepare.sh
+++ deploy/scripts/prepare.sh
@@ -6,108 +6,126 @@ FORCE="0"
 APP_SEVERNAME=""
 APP_ID=""
 
-printHelp() {    
-    echo "$0 argmuments:"    
-    echo "  -id <appid>         app identifier ( ie. mytest )"    
-    echo "  -sn <servername>    nginx app servername ( ie. mytest.searchathing.local )"
-    echo "  -f                  force overwrite existing"
+printHelp() {
+	echo "$0 argmuments:"
+	echo "  -id <appid>         app identifier ( ie. mytest )"
+	echo "  -sn <servername>    nginx app servername ( ie. mytest.searchathing.com )"
+	echo "  -sd <serverdomain>  nginx app serverdomain ( ie. searchathing.com )"
+	echo "  -f                  force overwrite existing"
 }
 
 mandatoryArg() {
-    if [ "$1" == "" ]; then
-        printHelp
-        exit 10
-    fi
+	if [ "$1" == "" ]; then
+		printHelp
+		exit 10
+	fi
 }
 
 while [ "$1" != "" ]; do
 
-    if [ "$1" == "--help" ]; then
-        printHelp
-        exit 1
-    fi    
+	if [ "$1" == "--help" ]; then
+		printHelp
+		exit 1
+	fi
 
-    if [ "$1" == "-f" ]; then
-        FORCE="1"
-        shift
-    fi    
+	if [ "$1" == "-f" ]; then
+		FORCE="1"
+		shift
+	fi
 
-     if [ "$1" == "-sn" ]; then
-        shift
-        mandatoryArg "$1"
-        APP_SERVERNAME=$1
-        shift
-        continue
-    fi
+	if [ "$1" == "-sn" ]; then
+		shift
+		mandatoryArg "$1"
+		APP_SERVERNAME=$1
+		shift
+		continue
+	fi
 
-    if [ "$1" == "-id" ]; then
-        shift
-        mandatoryArg  "$1"
-        APP_ID=$1
-        shift
-        continue
-    fi
+	if [ "$1" == "-sd" ]; then
+		shift
+		mandatoryArg "$1"
+		APP_SERVERDOMAIN=$1
+		shift
+		continue
+	fi
 
-    if [ "$1" != "" ]; then
-        echo "Unknown arg $1"
-        printHelp
-        exit 1
-    fi
+	if [ "$1" == "-id" ]; then
+		shift
+		mandatoryArg "$1"
+		APP_ID=$1
+		shift
+		continue
+	fi
+
+	if [ "$1" != "" ]; then
+		echo "Unknown arg $1"
+		printHelp
+		exit 1
+	fi
 
 done
 
-if [ "$APP_SERVERNAME" == "" ] || [ "$APP_ID" == "" ]; then
-    mandatoryArg
+if [ "$APP_SERVERNAME" == "" ] || [ "$APP_SERVERDOMAIN" == "" ] || [ "$APP_ID" == "" ]; then
+	mandatoryArg
 fi
 
 #--- services
 
 SERVICES=(
-    webapp.service    
+	webapp.service
 )
 
 for i in ${SERVICES[@]}; do
 
-    if [ "$FORCE" == "1" ] || [ ! -e /etc/systemd/system/$APP_ID-$i ]; then
+	if [ "$FORCE" == "1" ] || [ ! -e /etc/systemd/system/$APP_ID-$i ]; then
 
-        cp -v /root/deploy/$APP_ID/service/$i /etc/systemd/system/$APP_ID-$i
+		cp -v /root/deploy/$APP_ID/service/$i /etc/systemd/system/$APP_ID-$i
 
-        systemctl enable $APP_ID-$i
+		systemctl enable $APP_ID-$i
 
-    fi
+	fi
 
 done
+
+#--- security env
+
+if [ ! -e /root/security/$APP_ID.env ]; then
+	mkdir -p /root/security
+	cp -v /root/deploy/$APP_ID/webapp.env /root/security/$APP_ID.env
+fi
 
 #--- nginx
 
 if [ ! -e /etc/nginx/conf.d ]; then
-    echo "nginx missing ( solve with apt install nginx )"
-    exit 10
+	echo "nginx not found ( skipping )"
+else
+
+	NGINX_FILES=(
+		webapp.conf
+	)
+
+	for i in ${NGINX_FILES[@]}; do
+
+		RESTART_NGINX=false
+
+		if [ "$FORCE" == "1" ] || [ ! -e /etc/nginx/conf.d/$APP_ID-$i ]; then
+
+			DSTCONF="/etc/nginx/conf.d/$APP_ID-$i"
+
+			cp -v /root/deploy/$APP_ID/nginx/prod/$i "$DSTCONF"
+
+			sed -i "s/\stest\.searchathing\.com/ $APP_SERVERNAME/g" "$DSTCONF"
+			sed -i "s/\/searchathing\.com/\/$APP_SERVERDOMAIN/g" "$DSTCONF"
+			sed -i "s/webapp-test\.access/$APP_ID.access/g" "$DSTCONF"
+			sed -i "s/webapp-test\.error/$APP_ID.error/g" "$DSTCONF"
+			sed -i "/#>/d" "$DSTCONF"
+
+			RESTART_NGINX=true
+
+		fi
+
+		if $RESTART_NGINX; then service nginx restart; fi
+
+	done
+
 fi
-
-NGINX_FILES=(
-    webapp.conf
-)
-
-for i in ${NGINX_FILES[@]}; do
-
-    RESTART_NGINX=false
-
-    if [ "$FORCE" == "1" ] || [ ! -e /etc/nginx/conf.d/$APP_ID-$i ]; then
-
-        DSTCONF="/etc/nginx/conf.d/$APP_ID-$i"
-
-        cp -v /root/deploy/$APP_ID/nginx/prod/$i "$DSTCONF"
-
-        sed -i "s/\stest\.searchathing\.com/ $APP_SERVERNAME/g" "$DSTCONF"
-        sed -i "s/webapp-test\.access/$APP_ID.access/g" "$DSTCONF"
-        sed -i "s/webapp-test\.error/$APP_ID.error/g" "$DSTCONF"
-        sed -i "/#>/d" "$DSTCONF"
-
-        RESTART_NGINX=true
-
-    fi
-
-    if $RESTART_NGINX; then service nginx restart; fi
-
-done
--- publish.sh
+++ publish.sh
@@ -14,7 +14,8 @@ APP_ID=""
 printHelp() {
     echo "$0 argmuments:"
     echo "  -h <sshhost>        ssh host where to publish ( ie. main-test )"
-    echo "  -sn <servername>    nginx app servername ( ie. mytest.searchathing.local )"
+    echo "  -sn <servername>    nginx app servername ( ie. mytest.searchathing.com )"
+	echo "  -sd <serverdomain>  nginx app serverdomain ( ie. searchathing.com )"
     echo "  -id <appid>         app identifier ( ie. mytest )"    
     echo "  -f                  force overwrite existing"
 }
@@ -40,7 +41,7 @@ while [ "$1" != "" ]; do
     fi
 
     if [ "$1" == "-f" ]; then
-        FORCE="1"
+        FORCE="1"		
         shift
         continue
     fi
@@ -61,6 +62,14 @@ while [ "$1" != "" ]; do
         continue
     fi
 
+	if [ "$1" == "-sd" ]; then
+        shift
+        mandatoryArg "$1"
+        APP_SERVERDOMAIN=$1
+        shift
+        continue
+    fi
+
     if [ "$1" == "-id" ]; then
         shift
         mandatoryArg "$1"
@@ -77,7 +86,7 @@ while [ "$1" != "" ]; do
 
 done
 
-if [ "$APP_SSH_HOST" == "" ] || [ "$APP_SERVERNAME" == "" ] || [ "$APP_ID" == "" ]; then
+if [ "$APP_SSH_HOST" == "" ] || [ "$APP_SERVERNAME" == "" ] || [ "$APP_SERVERDOMAIN" == "" ] || [ "$APP_ID" == "" ]; then
     mandatoryArg
 fi
 
@@ -96,6 +105,9 @@ fi
 header "PROD ENV"
 # ======================================================================
 
+export APP_SERVERNAME="$APP_SERVERNAME"
+export FRONTEND_SRCDIR="$FRONTEND_SRCDIR"
+
 "$exdir"/frontend-publish-prepare.sh
 
 # ======================================================================
@@ -110,7 +122,7 @@ forceargs=""
 
 if [ "$FORCE" == "1" ]; then forceargs="-f"; fi
 
-ssh $APP_SSH_HOST "$REMOTE_DEPLOY/scripts/prepare.sh" -sn "$APP_SERVERNAME" -id "$APP_ID" "$forceargs"
+ssh $APP_SSH_HOST "$REMOTE_DEPLOY/scripts/prepare.sh" -sn "$APP_SERVERNAME" -id "$APP_ID" -sd "$APP_SERVERDOMAIN" "$forceargs"
 excode="$?"
 if [ "$excode" == "10" ]; then
     echo "some prerequisites missing"
