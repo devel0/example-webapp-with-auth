48836a2111a126fd6dcd9026a8621b83a5e22bfc
Lorenzo Delana <lorenzo.delana@gmail.com>
2025-10-19 17:29:20 +0200

    publish script

--- frontend-publish-prepare.sh
+++ frontend-publish-prepare.sh
@@ -1,24 +1,19 @@
 #!/bin/sh
 
+if [ "$APP_SERVERNAME" == "" ]; then
+	echo "missing APP_SERVERNAME var"
+	exit 10
+fi
+
+if [ "$FRONTEND_SRCDIR" == "" ]; then
+	echo "missing FRONTEND_SRCDIR var"
+	exit 10
+fi
+
 exdir=$(dirname "$(readlink -f "$BASH_SOURCE")")
-
-echo "APP_SERVERNAME=$APP_SERVERNAME"
-echo "FRONTEND_SRCDIR=$FRONTEND_SRCDIR"
-
-BACKEND_SRCDIR="$exdir"/src/backend
 
 GITCOMMIT="$(git rev-parse --short HEAD)"
 GITCOMMITDATE="$(git show -s --format="%ci" $GITCOMMIT)"
-
-# backend appsettings
-PRODFILE="$BACKEND_SRCDIR"/appsettings.Production.json
-PRODFILE_TMP="$BACKEND_SRCDIR"/appsettings.Production.json.tmp
-cat "$PRODFILE" |
-    jq ".GitCommit |= \"$GITCOMMIT\"" |
-    jq ".GitCommitDate |= \"$GITCOMMITDATE\"" |
-    jq ".AppServerName |= \"$APP_SERVERNAME\"" \
-        >"$PRODFILE_TMP"
-mv -f "$PRODFILE_TMP" "$PRODFILE"
 
 # frontend vite env
 
