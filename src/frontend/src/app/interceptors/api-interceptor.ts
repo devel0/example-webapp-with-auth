29e3b4ba8fa001cc2372c5a9c16825ad1cd4c749
Lorenzo Delana <lorenzo.delana@gmail.com>
2025-10-24 19:58:46 +0200

    snack network error

--- src/frontend/src/app/interceptors/api-interceptor.ts
+++ src/frontend/src/app/interceptors/api-interceptor.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { finalize, Observable, tap } from 'rxjs';
 import { GlobalService } from '../services/global-service';
+import { SnackService } from '../services/snack-service';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
 
   constructor(
-    private readonly globalService: GlobalService
+    private readonly globalService: GlobalService,
+    private readonly snackService: SnackService
   ) {
 
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let statusCode: number | null = null
-    let gotError : HttpErrorResponse | null = null
+    let gotError: HttpErrorResponse | null = null
 
     this.globalService.incGeneralNetwork()
 
@@ -30,6 +32,7 @@ export class ApiInterceptor implements HttpInterceptor {
           if (error instanceof HttpErrorResponse) {
             statusCode = error.status;
             gotError = error
+            this.snackService.showError(`Network error`, `${error.statusText}`)
           }
         }
       }),
