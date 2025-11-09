f99e7230d431285a58fd2c2f4fe5543a5110b842
Lorenzo Delana <lorenzo.delana@gmail.com>
2025-11-09 16:28:28 +0100

    api generated

--- src/frontend/src/api/api/api.ts
+++ src/frontend/src/api/api/api.ts
@@ -1,5 +1,7 @@
 export * from './auth.service';
 import { AuthApiService } from './auth.service';
+export * from './fakeData.service';
+import { FakeDataApiService } from './fakeData.service';
 export * from './main.service';
 import { MainApiService } from './main.service';
-export const APIS = [AuthApiService, MainApiService];
+export const APIS = [AuthApiService, FakeDataApiService, MainApiService];
